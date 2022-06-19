c10e861ed7ce961bd2c5e983e72f72b888ddebdd
Russel Arbore <russel.jma@gmail.com>
2022-06-19 12:51:35 -0500

    Simple copy buffer example

--- src/main.rs
+++ src/main.rs
@@ -12,20 +12,26 @@
  * along with vtrace-rs. If not, see <https://www.gnu.org/licenses/>.
  */
 
+use vulkano::buffer::*;
+use vulkano::command_buffer::*;
 use vulkano::device::physical::*;
 use vulkano::device::*;
 use vulkano::instance::*;
+use vulkano::sync::{self, *};
 
 fn main() {
     let instance = Instance::new(InstanceCreateInfo::default())
         .expect("ERROR: Couldn't create Vulkan instance.");
+
     let physical = PhysicalDevice::enumerate(&instance)
         .next()
         .expect("ERROR: No physical device is available.");
+
     let queue_family = physical
         .queue_families()
-        .find(|&q| q.supports_graphics())
+        .find(|&q| q.supports_graphics() && q.supports_compute())
         .expect("ERROR: Couldn't find a graphical queue family.");
+
     let (device, mut queues) = Device::new(
         physical,
         DeviceCreateInfo {
@@ -34,7 +40,43 @@ fn main() {
         },
     )
     .expect("ERROR: Couldn't create logical device.");
+
     let queue = queues
         .next()
         .expect("ERROR: No queues given to logical device.");
+
+    let src_contents = 0..256;
+    let src_buffer =
+        CpuAccessibleBuffer::from_iter(device.clone(), BufferUsage::all(), false, src_contents)
+            .expect("Error: Failed to create buffer.");
+
+    let dst_contents = (0..256).map(|_| 0);
+    let dst_buffer =
+        CpuAccessibleBuffer::from_iter(device.clone(), BufferUsage::all(), false, dst_contents)
+            .expect("Error: Failed to create buffer.");
+
+    let mut builder = AutoCommandBufferBuilder::primary(
+        device.clone(),
+        queue.family(),
+        CommandBufferUsage::OneTimeSubmit,
+    )
+    .unwrap();
+
+    builder
+        .copy_buffer(src_buffer.clone(), dst_buffer.clone())
+        .unwrap();
+
+    let command_buffer = builder.build().unwrap();
+
+    let future = sync::now(device.clone())
+        .then_execute(queue.clone(), command_buffer)
+        .unwrap()
+        .then_signal_fence_and_flush()
+        .unwrap();
+    future.wait(None).unwrap();
+
+    let src_content = src_buffer.read().unwrap();
+    let dst_content = dst_buffer.read().unwrap();
+    assert_eq!(&*src_content, &*dst_content);
+    println!("SUCCESS");
 }
