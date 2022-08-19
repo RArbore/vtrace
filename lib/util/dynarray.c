5f363f469734a2200e24abe85f72e32a2e44d9e4
Russel Arbore <russel.jma@gmail.com>
2022-08-18 23:40:30 -0500

    Add asserts for dynamic array interactions

--- lib/util/dynarray.c
+++ lib/util/dynarray.c
@@ -14,18 +14,29 @@
 
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 #include <stdio.h>
 
 #include "dynarray.h"
 
 result dynarray_create(uint32_t elem_size, uint32_t init_alloc_num, dynarray* dynarray) {
-    if (init_alloc_num == 0 || elem_size == 0) return CUSTOM_ERROR;
+    if (init_alloc_num == 0 || elem_size == 0) {
+#ifndef RELEASE
+	assert(0 && "ERROR: When a dynarray is created, you must allocate at least one element and the element size must be greater than 0");
+#endif
+	return CUSTOM_ERROR;
+    }
     dynarray->size = 0;
     dynarray->alloc = init_alloc_num * elem_size;
     dynarray->elem_size = elem_size;
 
     dynarray->data = malloc(dynarray->alloc);
-    if (!dynarray->data) return CUSTOM_ERROR;
+    if (!dynarray->data) {
+#ifndef RELEASE
+	assert(0 && "ERROR: malloc failed when initializing dynarray");
+#endif
+	return CUSTOM_ERROR;
+    }
     return SUCCESS;
 }
 
@@ -39,13 +50,21 @@ dynarray dynarray_create_singleton(void* contents, uint32_t elem_size) {
 }
 
 result dynarray_push(void* push_data, dynarray* dynarray) {
-    if (dynarray->alloc == 0) return CUSTOM_ERROR;
+    if (dynarray->alloc == 0) {
+#ifndef RELEASE
+	assert(0 && "ERROR: Array created with 0 allocated memory - this is probably a singleton array, which can't be pushed to");
+#endif
+	return CUSTOM_ERROR;
+    }
     if (dynarray->size >= dynarray->alloc) {
 	dynarray->alloc *= 2;
 	void* attempt = realloc(dynarray->data, dynarray->alloc);
 	if (!attempt) {
 	    dynarray->alloc = 0;
 	    free(dynarray->data);
+#ifndef RELEASE
+	    assert(0 && "ERROR: realloc failed when pushing to dynarray");
+#endif
 	    return CUSTOM_ERROR;
 	}
 	dynarray->data = attempt;
@@ -62,7 +81,12 @@ result dynarray_push(void* push_data, dynarray* dynarray) {
 }
 
 result dynarray_pop(void* pop_data, dynarray* dynarray) {
-    if (dynarray->size < dynarray->elem_size) return CUSTOM_ERROR;
+    if (dynarray->size < dynarray->elem_size) {
+#ifndef RELEASE
+	assert(0 && "ERROR: Tried to pop from an empty dynarray");
+#endif
+	return CUSTOM_ERROR;
+    }
     dynarray->size -= dynarray->elem_size;
     if (pop_data) memcpy(pop_data, (char*) dynarray->data + dynarray->size, dynarray->elem_size);
     return SUCCESS;
@@ -70,6 +94,7 @@ result dynarray_pop(void* pop_data, dynarray* dynarray) {
 
 result dynarray_clear(dynarray* dynarray) {
     dynarray->size = 0;
+    return SUCCESS;
 }
 
 uint32_t dynarray_len(dynarray* dynarray) {
@@ -77,18 +102,48 @@ uint32_t dynarray_len(dynarray* dynarray) {
 }
 
 void* dynarray_index(uint32_t index, dynarray* dynarray) {
-    return index < dynarray_len(dynarray) ? (void*) ((char*) dynarray->data + index * dynarray->elem_size) : NULL;
+    if (index < dynarray_len(dynarray)) {
+	return (void*) ((char*) dynarray->data + index * dynarray->elem_size);
+    }
+    else {
+#ifndef RELEASE
+	assert(0 && "ERROR: Index out of range of array");
+#endif
+	return NULL;
+    }
 }
 
 void* dynarray_first(dynarray* dynarray) {
-    return dynarray->size >= dynarray->elem_size ? dynarray_index(0, dynarray) : NULL;
+    if (dynarray->size >= dynarray->elem_size) {
+	return dynarray_index(0, dynarray);
+    }
+    else {
+#ifndef RELEASE
+	assert(0 && "ERROR: Can't get first element of empty array");
+#endif
+	return NULL;
+    }
 }
 
 void* dynarray_last(dynarray* dynarray) {
-    return dynarray->size >= dynarray->elem_size ? dynarray_index(dynarray_len(dynarray) - 1, dynarray) : NULL;
+    if (dynarray->size >= dynarray->elem_size) {
+	return dynarray_index(dynarray_len(dynarray) - 1, dynarray);
+    }
+    else {
+#ifndef RELEASE
+	assert(0 && "ERROR: Can't get last element of empty array");
+#endif
+	return NULL;
+    }
 }
 
 result dynarray_destroy(dynarray* dynarray) {
+    if (dynarray->alloc == 0) {
+#ifndef RELEASE
+	assert(0 && "ERROR: Can't destroy array with no allocated memory (singleton arrays don't own referenced memory)");
+#endif
+	return CUSTOM_ERROR;
+    }
     free(dynarray->data);
     return SUCCESS;
 }
